23b0d4316b9bd54f06b0a71621f23043e944d068
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2017-10-12 16:48:47 -0300

    removed unncesary code

--- spec/bsale_spec.rb
+++ spec/bsale_spec.rb
@@ -3,12 +3,12 @@ require "spec_helper"
 RSpec.describe Bsale do
   before(:each) do
     Bsale.configure do |config|
-      config.access_token = '1b39dfc11de21a39c248c0af23a66a868c4d9341'
+      config.access_token = ENV['BSALE_KEY']
     end
   end
 
   it 'set credentials correctly' do
-      expect(Bsale.config.access_token).to eq '1b39dfc11de21a39c248c0af23a66a868c4d9341'
+      expect(Bsale.config.access_token).to eq ENV['BSALE_KEY']
       expect(Bsale.config.content_type).to eq 'application/json'
     end
 
